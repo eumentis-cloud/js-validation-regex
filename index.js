4d3be4f808d0ebb00f3640ed44d1d3177495869a
Pawan Samdani <pawan.samdani@gmail.com>
2018-05-14 15:58:01 +0530

    v1.1.0

--- index.js
+++ index.js
@@ -9,4 +9,6 @@ module.exports = {
   path: /^[a-zA-Z\d-_/.]+$/,
   float: /^\d+(\.\d+)?$/,
   dateString: /^\d{4}-\d{2}-\d{2}$/,
+  charactersOnly: /^[a-zA-Z]*$/,
+  alphanumeric: /^[a-zA-Z\d]*$/,
 };
