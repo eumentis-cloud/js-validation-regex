a2b78071b0f6210ba4ce855cd2ca8aa809c7b97f
Pawan Samdani <pawan.samdani@gmail.com>
2018-07-28 14:00:56 +0530

    Added 'address' validation regex

--- index.js
+++ index.js
@@ -13,4 +13,5 @@ module.exports = {
   alphanumeric: /^[a-zA-Z\d]*$/,
   ascii: /^[\x20-\x7E]*$/,
   string: /^[\w\x20-\x2F\x2C-\x2E\x3A\x3B\x3F\x40\x5B\x5D\x60\x7C\x7E]*$/,
+  address: /^[a-zA-Z\d,.\x2D\x20\n\r]*$/,
 };
