a1448c60d9ee9d4b817f4e0c1da228409180e377
Pawan Samdani <pawan.samdani@gmail.com>
2018-10-20 10:36:29 +0530

    Added 'gstin' regex

--- index.js
+++ index.js
@@ -14,4 +14,5 @@ module.exports = {
   ascii: /^[\x20-\x7E]*$/,
   string: /^[\w\x20-\x2F\x2C-\x2E\x3A\x3B\x3F\x40\x5B\x5D\x60\x7C\x7E]*$/,
   address: /^[a-zA-Z\d,."'\x2D\x20\n\r]*$/,
+  gstin: /^\d{2}[A-Z]{5}\d{4}[A-Z][a-zA-Z\d]Z[a-zA-Z\d]$/,
 };
